1e64af50140557ec0544236802bbe70a642ac753
fuzzyduck <174277889+fuzzyduck03a@users.noreply.github.com>
2024-10-17 13:07:29 -0600

    Initial Setup

---
+++ app/routes/_index.tsx
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Negotiate Fun" },
+    { name: "description", content: "A tool to aid kink negotiation and self-reflection" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={
+      {
+        padding: "2rem",
+        backgroundColor: "#5D3557",
+        color: "white",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }
+    }>
+      <h1>Negotiate Fun</h1>
+      <p>
+        A tool to aid kink negotiation and self-reflection
+      </p>
+      <h3>Coming Soon</h3>
+      <a href={`${import.meta.env.BASE_URL}`}>Source Repository</a>
+      <p>
+        Version: {`${import.meta.env.DVERSION}`}
+      </p>
+    </div>
+  );
+}
